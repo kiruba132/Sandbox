e1337521a70a8dacdc888a09f8d199ba2122ceed
Gaming PC <sdf@gmail.com>
2024-06-02 18:28:29 +1000

    previous version fixed

--- password_stars.py
+++ password_stars.py
@@ -2,8 +2,9 @@ MINIMUM_LENGTH = 10
 
 
 def get_password():
-    password = input(f"Password too short! It must be at least {MINIMUM_LENGTH} characters.")
+    password = input(f"Enter a password of at least {MINIMUM_LENGTH} characters: ")
     while len(password) < MINIMUM_LENGTH:
+        print(f"Password too short! It must be at least {MINIMUM_LENGTH} characters.")
         password = input(f"Enter a password of at least {MINIMUM_LENGTH} characters: ")
 
     print('*' * len(password))
