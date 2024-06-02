4b687a50f02af4471b1221703909ac41c2408d9c
Gaming PC <sdf@gmail.com>
2024-06-02 18:21:19 +1000

    Updated for better naming convention

--- password_stars.py
+++ password_stars.py
@@ -1,14 +1,13 @@
 MINIMUM_LENGTH = 10
 
 
-def version_1():
-    """Get a password of valid size and print asterisks."""
-    password = input(f"Enter password of at least {MINIMUM_LENGTH} characters: ")
+def get_password():
+    password = input(f"Password too short! It must be at least {MINIMUM_LENGTH} characters.")
     while len(password) < MINIMUM_LENGTH:
-        password = input(f"Enter password of at least {MINIMUM_LENGTH} characters: ")
+        password = input(f"Enter a password of at least {MINIMUM_LENGTH} characters: ")
 
     print('*' * len(password))
 
 
 # Call the function to execute
-version_1()
+get_password()
